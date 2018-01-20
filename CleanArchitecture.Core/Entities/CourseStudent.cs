fbdf9a2a4c60603b49620c4b00fdefe051715973
betoramiz <maramirez10@gmail.com>
2018-01-19 21:37:06 -0600

    added many to many relationship

---
+++ CleanArchitecture.Core/Entities/CourseStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class CourseStudent
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
